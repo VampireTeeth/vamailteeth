afe84e0742178ed7551591d09e09497005a9422f
Steven liu <steven.liu@macquarie.com>
2017-09-11 17:39:23 +1000

    Main class added

---
+++ src/main/java/tech/vampireteeth/vamailteeth/Vamailteeth.java
@@ -0,0 +1,19 @@
+package tech.vampireteeth.vamailteeth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Vamailteeth {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello, world!";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Vamailteeth.class, args);
+    }
+}
