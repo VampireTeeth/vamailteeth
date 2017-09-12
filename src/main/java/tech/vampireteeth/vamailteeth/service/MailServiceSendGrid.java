7ffc88a1b5c628c9dbba8b2576c37eab8ecfb567
VampireTeeth <vampireteeth001@gmail.com>
2017-09-12 22:57:14 +1000

    Adding MailServiceSendGrid skeleton

---
+++ src/main/java/tech/vampireteeth/vamailteeth/service/MailServiceSendGrid.java
@@ -0,0 +1,14 @@
+package tech.vampireteeth.vamailteeth.service;
+
+import tech.vampireteeth.vamailteeth.model.MailRequest;
+import tech.vampireteeth.vamailteeth.model.MailResponse;
+
+public class MailServiceSendGrid implements MailService {
+
+	@Override
+	public MailResponse sendMail(MailRequest mailRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
