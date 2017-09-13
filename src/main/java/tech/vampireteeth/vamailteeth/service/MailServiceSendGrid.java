559ce802403778cb08346db07741f0b2a3761bd3
VampireTeeth <vampireteeth001@gmail.com>
2017-09-14 06:52:09 +1000

    remove the API key in the source code

--- src/main/java/tech/vampireteeth/vamailteeth/service/MailServiceSendGrid.java
+++ src/main/java/tech/vampireteeth/vamailteeth/service/MailServiceSendGrid.java
@@ -8,8 +8,6 @@ import tech.vampireteeth.vamailteeth.model.MailResponse;
 @Service("mailServiceSendGrid")
 public class MailServiceSendGrid implements MailService {
 
-    private static final String API_KEY = "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";
-
     @Override
     public MailResponse sendMail(MailRequest mailRequest) {
         // TODO Auto-generated method stub
